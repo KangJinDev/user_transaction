4df6c842bd4c92d03796ae666b795710fa192532
KangJinDev <sws092298@outlook.com>
2021-02-19 12:02:25 -0500

    make customerdetail component

--- client/src/components/Customer/CustomerDetails.js
+++ client/src/components/Customer/CustomerDetails.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { updateCustomer } from '../../redux/actions/customerAction';
-import {connect } from 'react-redux';
+import { connect } from 'react-redux';
 import history from '../../redux/history'
-
 class CustomerDetails extends React.Component {
-    constructor(props){
-        super(props);
-        const location = history.location
-        this.state = location.state;
-        this.handleUpdate = this.handleUpdate.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    const location = history.location
+    this.state = location.state;
+    this.handleUpdate = this.handleUpdate.bind(this);
+  }
 
-handleChangeFor = (propertyName) => (event) => {
+  handleChangeFor = (propertyName) => (event) => {
     const { customer } = this.state;
     const customerDetails = {
       ...customer,
@@ -27,58 +26,124 @@ handleChangeFor = (propertyName) => (event) => {
     this.props.updateCustomer(this.state.customer);
   }
 
-  render(){
+  render() {
 
-    return(
-        <div className="customerDetail">
-            <h2>Customer Detail</h2>
-                <div>
-                </div>
-            {
-                <form onSubmit={this.handleUpdate}>
-                <div className="form-group">
-                  <label htmlFor="firstName">First Name</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="firstName"
-                    name="firstName"
-                    autoComplete="off"
-                    onChange={this.handleChangeFor('firstName')} value={this.state.customer.firstName}
-                    />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="lastName">Last Name</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="lastName"
-                    name="lastName"
-                    autoComplete="off"
-                    onChange={this.handleChangeFor('lastName')} value={this.state.customer.lastName}
-                    />
-                </div>
-                <button type="submit" className="btn btn-success btn-lg">
-                  UPDATE
-                </button>
-               </form>
+    return (
+      <div className="customerDetail containter">
+        <h2>Customer Detail</h2>
+        {
 
-              }
-            
-        </div>
+          <form onSubmit={this.handleUpdate}>
+            <div className="form-group">
+              <label htmlFor="name">Customer Name</label>
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                name="name"
+                autoComplete="off"
+                onChange={this.handleChangeFor('name')} value={this.state.customer.name}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="description">Description</label>
+              <input
+                type="text"
+                className="form-control"
+                id="description"
+                name="description"
+                autoComplete="off"
+                onChange={this.handleChangeFor('description')} value={this.state.customer.description}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                className="form-control"
+                id="email"
+                name="email"
+                autoComplete="off"
+                onChange={this.handleChangeFor('email')} value={this.state.customer.email}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="lastName">Bitcoin Wallet Id</label>
+              <input
+                type="text"
+                className="form-control"
+                id="btc_wallet_id"
+                name="btc_wallet_id"
+                autoComplete="off"
+                onChange={this.handleChangeFor('btc_wallet_id')} value={this.state.customer.btc_wallet_id}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="btc_wallet_balance">Bitcoin Wallet balance</label>
+              <input
+                type="text"
+                className="form-control"
+                id="btc_wallet_balance"
+                name="btc_wallet_balance"
+                autoComplete="off"
+                onChange={this.handleChangeFor('btc_wallet_balance')} value={this.state.customer.btc_wallet_balance}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="eth_wallet_id">Ethereum Wallet Id</label>
+              <input
+                type="text"
+                className="form-control"
+                id="eth_wallet_id"
+                name="eth_wallet_id"
+                autoComplete="off"
+                onChange={this.handleChangeFor('eth_wallet_id')} value={this.state.customer.eth_wallet_id}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="eth_wallet_balance">Ethereum Wallet balance</label>
+              <input
+                type="text"
+                className="form-control"
+                id="eth_wallet_balance"
+                name="eth_wallet_balance"
+                autoComplete="off"
+                onChange={this.handleChangeFor('eth_wallet_balance')} value={this.state.customer.eth_wallet_balance}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="max_amount">Max Amount</label>
+              <input
+                type="text"
+                className="form-control"
+                id="max_amount"
+                name="max_amount"
+                autoComplete="off"
+                onChange={this.handleChangeFor('max_amount')} value={this.state.customer.max_amount}
+              />
+            </div>
+            <button type="submit" className="btn btn-success btn-lg">
+              UPDATE
+            </button>
+          </form>
+
+        }
+
+      </div>
+
     );
   }
 }
 
 const mapStateToProps = (state) => {
-    return {
-      customers: state.customers
-    }
+  return {
+    customers: state.customers
   }
-  const mapDispatchToProps = (dispatch) => {
-    return {
-    updateCustomer : customer => dispatch(updateCustomer(customer))
-    }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateCustomer: customer => dispatch(updateCustomer(customer))
   }
-  
-  export default connect(mapStateToProps,mapDispatchToProps)(CustomerDetails);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CustomerDetails);
